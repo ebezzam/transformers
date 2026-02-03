6d113acb55d50cb618dae4bc86ddeeb2005f3de0
Eric B <ebezzam@gmail.com>
2026-02-03 13:31:16 +0100

    Nit

--- tests/models/vibevoice_acoustic_tokenizer/test_modeling_vibevoice_acoustic_tokenizer.py
+++ tests/models/vibevoice_acoustic_tokenizer/test_modeling_vibevoice_acoustic_tokenizer.py
@@ -168,10 +168,6 @@ class VibeVoiceAcousticTokenizerModelTest(ModelTesterMixin, unittest.TestCase):
     def test_hidden_states_output(self):
         pass
 
-    # @unittest.skip(reason="From CI 'UnboundLocalError: local variable output referenced before assignment'")
-    # def test_model_parallelism(self):
-    #     pass
-
     def test_determinism(self):
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
 
